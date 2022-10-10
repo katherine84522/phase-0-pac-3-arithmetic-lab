2954ee70c4f8d2cb5140a271f2499f88fabda659
Katherine Tsai <katherinetsai@DESKTOP-10SDEV1.localdomain>
2022-10-10 17:41:01 -0400

    commit tree fe3cfb5ac57ee3376d07aab7f98be2dfdb514b19

---
+++ index.js
@@ -0,0 +1,27 @@
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+function increment(number) {
+    return number += 1;
+}
+function decrement(number) {
+    return number -= 1;
+}
+function makeInt(a) {
+    return parseInt(a, 10);
+}
+function makeInt(a = 2) {
+    return parseInt(a, 10)
+}
+function preserveDecimal(a) {
+    return parseFloat(a);
+}
